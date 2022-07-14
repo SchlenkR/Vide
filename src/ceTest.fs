2e50136b5cf450e08f76693dc1011cb069d31b06
SchlenkR <ronald.schlenker@binarygears.de>
2022-07-14 10:16:45 +0200

    stuff

--- src/ceTest.fs
+++ src/ceTest.fs
@@ -1,15 +1,18 @@
 
+module Test
+
 type Builder() =
     member inline this.Yield(x) = [x]
     member inline _.Delay(f: unit -> _) = f
     member _.Combine(a, b) = List.append a (b())
     member _.Zero() = []
     member _.Run(children) = children()
-    member inline _.For(inputList: seq<'a>, body: 'a -> list<'b>) : seq<list<'b>> =
-        [ for x in inputList do body x ]
+    member inline _.For(inputList: seq<'a>, body: 'a -> list<'b>) : list<'b> =
+        [ for x in inputList do yield! body x ]
 let test = Builder()
 
-test { 
-    for x in 0..10 do
-        "xxx"
-}
+let res = 
+    test { 
+        for x in 0..10 do
+            string x
+    }
