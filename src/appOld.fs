b0b11483f7692f25c82ff8a49967db6e559006ed
SchlenkR <ronald.schlenker@binarygears.de>
2022-07-14 11:30:04 +0200

    x

--- src/appOld.fs
+++ src/appOld.fs
@@ -3,6 +3,7 @@
 //  - first class task/async support (in gen)
 //  - implement "for" in ChildBuilder
 //  - hide all the crazy generic type signatures
+// - use #Node instead of Node
 
 module App
 
@@ -38,50 +39,19 @@ module HelperAndExtensions =
 [<AutoOpen>]
 module Framework =
 
-    type AppGen<'o,'s> = Gen<'o,'s,App>
-    type RuntimeTypedAppGen<'o> = Type * AppGen<'o,obj>
+    type RuntimeTypedAppGen<'o> = Type * Gen<'o,obj,App>
 
-    let inline boxGen (stateType: Type) (Gen g: AppGen<'o,'s>) : RuntimeTypedAppGen<'o> =
-        // fable requires runtime-resolution and passing the stateType from callsite due to erasure
-        let g = Gen <| fun s r ->
-            let o,s = g (unbox s) r
-            o, box s
-        stateType, g
+    let inline syncAttributes (elem: Node) attributes =
+        do for aname,avalue in attributes do
+            let elemAttr = elem.attributes.getNamedItem aname
+            if elemAttr.value <> avalue then
+                elemAttr.value <- avalue
 
-    // TODO: Add overloads for yield (string, int, etc.)
-    type ChildrenBuilder<'o,'s>(run: RuntimeTypedAppGen<'o> list -> AppGen<'o,'s>) =
-        member inline _.Yield<'o,'s1>(x: Gen<'o,'s1,App>) = [boxGen typeof<'s1> x]
-        member inline _.YieldFrom<'o,'s1>(x: Gen<'o list,'s1,App>) = [boxGen typeof<'s1> x]
-        member inline _.Delay([<InlineIfLambda>] f) = f ()
-        member _.Combine(a, b) = List.append a b
-        member _.Zero() = []
-        member _.Run(children) = run children
-        member inline _.For(sequence: seq<'a>, body: 'a -> RuntimeTypedAppGen<'o> list) : RuntimeTypedAppGen<'o> list =
-            [ for x in sequence do yield! body x ]
-
-    type ViewBuilder() =
-        member inline _.Bind(m, [<InlineIfLambda>] f) = Gen.bind m f
-        member _.Yield(x: AppGen<'o,'s>) = x
-        member inline _.Delay([<InlineIfLambda>] f) = f ()
-        member inline _.Combine(a, b: Bottom) : Gen<_,_,_> = a
-        member _.Zero() = ()
-    and Bottom private () = class end
-    let pview = ViewBuilder()
-
-
-[<AutoOpen>]
-module HtmlElementsApi =
-
-    let inline elem name attributes children =
-        let syncAttributes (elem: Node) =
-            do for aname,avalue in attributes do
-                let elemAttr = elem.attributes.getNamedItem aname
-                if elemAttr.value <> avalue then
-                    elemAttr.value <- avalue
-        let syncChildren (elem: Node) = Gen <| fun s r ->
+    let inline syncChildren (elem: Node) (children: RuntimeTypedAppGen<_> list) =
+        Gen <| fun s r ->
             let s = s |> Option.defaultWith (fun () -> ResizeArray())
-            let newState = seq {
-                for childType, (Gen childGen) in children do
+            let newState =
+                [ for childType, (Gen childGen) in children do
                     let stateIdx = s |> Seq.tryFindIndex (fun (typ,_) -> typ = childType)
                     let newChildState =
                         match stateIdx with
@@ -94,14 +64,52 @@ module HtmlElementsApi =
                             do elem.appendChild o |> ignore
                             s
                     yield childType,newChildState
-            }
+                ]
             (), ResizeArray newState
+
+    let inline boxGen (stateType: Type) (Gen g: Gen<'o,'s,App>) : RuntimeTypedAppGen<'o> =
+        // fable requires runtime-resolution and passing the stateType from callsite due to erasure
+        let g = Gen <| fun s r ->
+            let o,s = g (unbox s) r
+            o, box s
+        stateType, g
+
+    // TODO: Add overloads for yield (string, int, etc.)
+    type ChildrenBuilder<'o,'s when 'o :> Node>(nodeGen: Gen<'o,'s,App>) =
+        member inline _.Yield<'o,'s1>(x: Gen<'o,'s1,App>) = [boxGen typeof<'s1> x]
+        member inline _.YieldFrom<'o,'s1>(x: Gen<'o list,'s1,App>) = [boxGen typeof<'s1> x]
+        member inline _.Delay([<InlineIfLambda>] f) = f ()
+        member _.Combine(a, b) = List.append a b
+        member _.Zero() = []
+        member _.Run<'o when 'o :> Node>(children: list<RuntimeTypedAppGen<'o>>) =
+            loop {
+                let! (node: Node) = nodeGen
+                do! syncChildren node children
+                return node
+            }
+
+        member inline _.For(sequence: seq<'a>, body: 'a -> RuntimeTypedAppGen<'o> list) : RuntimeTypedAppGen<'o> list =
+            [ for x in sequence do yield! body x ]
+
+    type ViewBuilder() =
+        member inline _.Bind(m, [<InlineIfLambda>] f) = Gen.bind m f
+        member _.Yield(x: Gen<#Node,'s,App>) = x
+        member inline _.Delay([<InlineIfLambda>] f) = f ()
+        member _.Combine(a, b: Bottom) : Gen<_,_,_> = a
+        member _.Zero() = Bottom.Instance
+    and Bottom private () = static member internal Instance = Bottom()
+    let pview = ViewBuilder()
+
+
+[<AutoOpen>]
+module HtmlElementsApi =
+
+    let inline elem name attributes =
         loop {
             let! app = app
             let! elem = preserve (fun () -> app.Document.createElement name :> Node)
             printfn $"Eval: {name} ({elem.GetHashCode()})"
-            do syncAttributes elem
-            do! syncChildren elem
+            do syncAttributes elem attributes
             return elem
         }
 
@@ -119,20 +127,19 @@ module HtmlElementsApi =
     let p attributes = ChildrenBuilder(elem "p" attributes)
 
     let button attributes click =
-        ChildrenBuilder(fun children -> loop {
+        ChildrenBuilder <| loop {
             let! app = app
-            // TODO: Optimize the map afterwards; that's not necessary
             let! button =
-                elem "button" attributes children
-                |> Gen.map (fun x -> x :?> HTMLButtonElement)
+                elem "button" attributes
+                |> Gen.map (fun x -> x :?> HTMLButtonElement) // TODO: Cast + map necessary?
             button.onclick <- fun _ ->
                 printfn "-----CLICK"
                 click ()
                 app.TriggerUpdate()
             return button :> Node // TODO: It's crap that we have to cast everything to "Node"
-        })
+        }
 
-    let nothing = text ""
+    let empty = text ""
 
 
 let comp =
