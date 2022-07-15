1ec1f1536515a152bf8156f8cd24827db91319bc
SchlenkR <ronald.schlenker@binarygears.de>
2022-07-15 22:40:01 +0200

    asd

--- src/ceTest.fs
+++ src/ceTest.fs
@@ -73,7 +73,10 @@ module Test
 
 open System
 
-type Gen<'o,'s> = { value: 'o; state: 's }
+type App = App
+type Gen<'o,'s,'r> = { value: 'o; state: 's }
+type AppGen<'o,'s> = Gen<'o,'s,App>
+type NoState = NoState
 
 type RTState = { stateTypeName: string; state: obj }
 
@@ -84,64 +87,63 @@ let toRTState (x: 'a) =
         | args ->
             let args = args |> Array.map getTypeName |> String.concat ", "
             $"{t.Name}<{args}>"
-    { stateTypeName = getTypeName typeof<'a>; state = x :> obj }
+    { stateTypeName = getTypeName typeof<'a>
+      state = x :> obj }
 
-let bind (m: Gen<'a,'s1>) (f: 'a -> Gen<'b,'s2>) : Gen<'b, 's1 * 's2> =
+let bind (m: AppGen<'a,'s1>) (f: 'a -> AppGen<'b,'s2>) : AppGen<'b,'s1*'s2> =
     let fres = f m.value
-    { value = fres.value; state = m.state,fres.state }
+    { value = fres.value
+      state = m.state,fres.state }
 
-let ofValue v = { value = v; state = () }
+
+let ofValue v : AppGen<_,_> = { value = v; state = NoState }
 
 type YieldedOrCombined<'a> = YieldedOrCombined of 'a list
 type Delayed<'a> = Delayed of 'a list
 
 type Builder() =
-    member inline _.Bind(m: Gen<'a,'s1>, f: 'a -> Gen<'b,'s2>) : Gen<'b,_> =
+    member inline _.Bind(m: AppGen<'a,'s1>, f: 'a -> AppGen<'b,'s2>) : AppGen<'b,_> =
         printfn $"BIND     -  m.value = {m.value}"
         let fres = bind m f
-        {
-            value = fres.value
-            state = toRTState (m.state, fres.state)
-        }
-    member _.Yield(x: Gen<'a,'s>) =
+        { value = fres.value
+          state = toRTState (m.state, fres.state) }
+    member _.Yield(x: Gen<'a,'s,_>) =
         printfn $"YIELD    -  x.value = {x.value}"
-        { 
-            value = YieldedOrCombined [x.value]
-            state = toRTState x.state
-        }
-    member _.Delay(f: unit -> Gen<YieldedOrCombined<'a>,RTState>) : Gen<Delayed<'a>,RTState> =
+        { value = YieldedOrCombined [x.value]
+          state = toRTState x.state }
+    member _.Delay(f: unit -> AppGen<YieldedOrCombined<'a>, RTState>) : AppGen<Delayed<'a>, RTState> =
         let fres = f()
         let (YieldedOrCombined fvalue) = fres.value
         printfn $"DELAY    -  f() = {fvalue}"
-        { 
-            value = Delayed fvalue
-            state = fres.state 
-        }
-    member _.Combine(a: Gen<YieldedOrCombined<'a>,'s>, b: Gen<Delayed<'a>,RTState>) =
+        { value = Delayed fvalue 
+          state = fres.state }
+    member _.Combine(a: AppGen<YieldedOrCombined<'a>, 's>, b: AppGen<Delayed<'a>, RTState>) =
         printfn $"COMBINE  -  a.value = {a.value}  -  b.value = {b.value}"
         let (YieldedOrCombined avalues) = a.value
         let (Delayed bvalues) = b.value
-        {
-            value = YieldedOrCombined (avalues @ bvalues)
-            state = toRTState (a.state, b.state)
-        }
+        { value = YieldedOrCombined (avalues @ bvalues)
+          state = toRTState (a.state, b.state) }
     member inline _.For(
             sequence: seq<'a>,
-            body: 'a -> Gen<YieldedOrCombined<'o>, _>
-            ) : Gen<YieldedOrCombined<'o>,_> =
-        [ for x in sequence do
-            yield! body x
-        ]
-    // member _.Zero() = 
-    //     printfn $"ZERO"
-    //     []
+            body: 'a -> AppGen<YieldedOrCombined<'o>, _>
+            ) : AppGen<YieldedOrCombined<'o>, _> =
+        failwith "TODO"
+        // [ for x in sequence do
+        //     yield! body x
+        // ]
+    member inline _.Zero() =
+        printfn $"ZERO"
+        { value = YieldedOrCombined []
+          state = toRTState NoState }
     // member _.Run(x) =
     //     printfn $"RUN      -   {x}"
     //     x
 
 let test = Builder()
 
-let x =
+
+
+let a =
     test {
         let! a = { value = 100; state = "a" }
         let! b = { value = 200; state = 44.2 }
@@ -157,16 +159,16 @@ let x =
 
         let! e = { value = -2; state = [909090] }
         let! f = { value = -3; state = (0.1, 0.2, 0.3) }
-        // for i in 0..3 do
-        //     { value = e + f + i; state = ("Hello", "World") }
-        //     { value = e + f + i; state = ("Hello", "World") }
-        //     { value = e + f + i; state = ("Hello", "World") }
+        for i in 0..3 do
+            { value = e + f + i; state = ("Hello", "World") }
+            { value = e + f + i; state = ("Hello", "World") }
+            { value = e + f + i; state = ("Hello", "World") }
 
         { value = e + f; state = ("Hello", "World") }
     }
 
 
-let y =
+let b =
     test {
         let! a = { value = 100; state = "a" }
         let! b = { value = 200; state = 44.2 }
@@ -176,4 +178,22 @@ let y =
         let! d = { value = 66; state = 44.1 }
         { value = c + d; state = 10.0 }
         { value = -77; state = 20.0 }
+    }
+
+
+let c =
+    test {
+        { value = 33; state = 20UL }
+        { value = -77; state = 20.0 }
+    }
+
+let d =
+    test {
+        { value = -77; state = 20.0 }
+    }
+
+let e =
+    test {
+        if true then
+            { value = -77; state = 20.0 }
     }
