6cddbd4c3cb3b330ef5b6832d4c23d3f95609748
SchlenkR <ronald.schlenker@binarygears.de>
2022-08-21 09:34:14 +0200

    demos

--- src/app.fs
+++ src/app.fs
@@ -12,50 +12,51 @@ let demos : list<string * string * (HTMLElement -> unit)> =
         let onEvaluated _ state = currentState <- state |> Option.map (fun s -> s :> obj)
         let videMachine = prepareStart host demo onEvaluated
         videMachine.Eval()
+    let demos =
+        [
+            (
+                "Hello World",
+                "Just a message to the world...",
+                start Demos.helloWorld
+            )
 
-    [
-        (
-            "Hello World",
-            "Just a message to the world...",
-            start Demos.helloWorld
-        )
-
-        (
-            "Counter",
-            "The famous, one-of-a kind counter.",
-            start Demos.counter
-        )
+            (
+                "Counter",
+                "The famous, one-of-a kind counter.",
+                start Demos.counter
+            )
         
-        (
-            "Conditional attributes",
-            "Count to 5 and you'll get a surprise!",
-            start Demos.conditionalAttributes
-        )
+            (
+                "Conditional attributes",
+                "Count to 5 and you'll get a surprise!",
+                start Demos.conditionalAttributes
+            )
 
-        (
-            "Conditional elements (multiple if)",
-            "Count to 5 and you'll get another surprise!",
-            start Demos.conditionalIfs
-        )
+            (
+                "Conditional elements (multiple if)",
+                "Count to 5 and you'll get another surprise!",
+                start Demos.conditionalIfs
+            )
 
-        (
-            "List of elements",
-            "Just an immutable list.",
-            start Demos.simpleFor
-        )
+            (
+                "List of elements",
+                "Just an immutable list.",
+                start Demos.simpleFor
+            )
 
-        (
-            "Mutable element list",
-            "Add / Remove items",
-            start Demos.statelessFor
-        )
+            (
+                "Mutable element list",
+                "Add / Remove items",
+                start Demos.statelessFor
+            )
 
-        (
-            "List with element state",
-            "TODO",
-            start Demos.statefulFor
-        )
-    ]
+            (
+                "List with element state",
+                "TODO",
+                start Demos.statefulFor
+            )
+        ]
+    demos
 
 let menu = document.getElementById("menu")
 let demoHost = document.getElementById("demo")
@@ -76,26 +77,26 @@ for title,desc,runDemo in demos do
     menu.appendChild(btn) |> ignore
 
 document.getElementById("logState").onclick <- fun _ ->
-    let isNode elem = typeof<HTMLElement>.IsInstanceOfType(elem)
-    let isArray elem = Array.isArray(elem)
-    let flattenedState =
-        match currentState with
-        | None -> []
-        | Some state ->
-            let rec flattenState (state: obj) =
-                [
-                    if isArray state then
-                        let arr = state :?> array<obj>
-                        let e0 = arr[0]
-                        let e1 = arr[1]
-                        if isNode e0 
-                            then yield (e0 :?> HTMLElement).tagName
-                            else yield JSON.stringify(e0)
-                        yield! flattenState e1
-                    else
-                        yield JSON.stringify(state)
-                ]
-            flattenState state
-    //console.log(flattenedState |> List.toArray)
-    //console.log(JSON.stringify(currentState, space = 2))
+    //let isNode elem = typeof<HTMLElement>.IsInstanceOfType(elem)
+    //let isArray elem = Array.isArray(elem)
+    //let flattenedState =
+    //    match currentState with
+    //    | None -> []
+    //    | Some state ->
+    //        let rec flattenState (state: obj) =
+    //            [
+    //                if isArray state then
+    //                    let arr = state :?> array<obj>
+    //                    let e0 = arr[0]
+    //                    let e1 = arr[1]
+    //                    if isNode e0 
+    //                        then yield (e0 :?> HTMLElement).tagName
+    //                        else yield JSON.stringify(e0)
+    //                    yield! flattenState e1
+    //                else
+    //                    yield JSON.stringify(state)
+    //            ]
+    //        flattenState state
+    ////console.log(flattenedState |> List.toArray)
+    ////console.log(JSON.stringify(currentState, space = 2))
     console.log(currentState)
