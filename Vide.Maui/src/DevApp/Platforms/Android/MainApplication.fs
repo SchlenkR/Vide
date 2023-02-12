b705e204b7f7e965f0386a733cdd2dc53575b165
SchlenkR <ronald@pure-state.de>
2023-02-12 18:27:11 +0100

    Vide.Maui init

---
+++ Vide.Maui/src/DevApp/Platforms/Android/MainApplication.fs
@@ -0,0 +1,13 @@
+﻿namespace DevApp
+
+open Android.App
+open Microsoft.Maui
+open DevApp
+
+[<Application>]
+type MainApplication(handle, ownership) =
+    inherit MauiApplication(handle, ownership)
+
+    do Resource.UpdateIdValues()
+    
+    override _.CreateMauiApp() = MauiProgram.CreateMauiApp()
---
+++ Vide.Maui/src/DevApp/Platforms/MacCatalyst/Program.fs
@@ -0,0 +1,9 @@
+﻿namespace DevApp
+
+open UIKit
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main(args, null, typeof<AppDelegate>)
+        0
