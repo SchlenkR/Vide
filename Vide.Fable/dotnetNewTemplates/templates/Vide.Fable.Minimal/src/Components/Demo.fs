f71cbd399d5deadd6900998513ae4f5502e6819e
SchlenkR <ronald@pure-state.de>
2023-04-08 17:22:47 +0200

    Template Cleanup

--- Vide.Fable/dotnetNewTemplates/templates/Vide.Fable.Minimal/src/Components/Demo.fs
+++ Vide.Fable/dotnetNewTemplates/templates/Vide.Fable.Minimal/src/Components/Demo.fs
@@ -1,8 +1,6 @@
 module Components.Demo
 
-open Fable.Core
 open Fable.Core.JsInterop
-open Browser
 open Vide
 open type Html
 
@@ -12,6 +10,7 @@ let view =
     vide {
         div.class' ("main-view") {
             p { img.src("./src/components/logo.png").width("150px") }
-            "The whole vide world"
+            hr
+            "The whole Vide world"
         }
     }
