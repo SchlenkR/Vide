e6f186d578fda7945d8f72b8c97f04051f1e6333
SchlenkR <ronald@pure-state.de>
2023-04-28 16:56:08 +0200

    #3

--- Vide.Fable/src/DevApp/src/UseCases/Issue_3.fs
+++ Vide.Fable/src/DevApp/src/UseCases/Issue_3.fs
@@ -18,7 +18,6 @@ module Issue_2 =
             | None -> Vide.elseForget
         }
 
-
     let cards() = vide {
         Card(
             vide {
@@ -55,7 +54,6 @@ module Issue_2a =
             | None -> Vide.elseForget
         }
 
-
     let cards = vide {
         Card(
             vide {
@@ -72,6 +70,44 @@ module Issue_2a =
     }
 
     let view = vide { article { main { cards } } }
+
+
+module Issue_2b =
+
+open Vide
+open type Vide.Html
+
+let Card
+    (
+        content: Vide<_,_,_>,
+        footerContent: Vide<_,_,_> option
+    ) =
+    vide {
+        div { content }
+        
+        match footerContent with
+        | Some footerContent -> header { footerContent }
+        | None -> Vide.elseForget
+    }
+
+let videNone : Vide<unit,unit,_> option = None
+
+let cards = vide {
+    Card(
+        vide {
+            let! counter = Vide.ofMutable 0
+            $"The current count is {counter.Value} :)"
+        },
+        Some (vide { "Footer is here" })
+    )
+
+    Card(
+        vide { "This is just another Usage" },
+        videNone
+    )
+}
+
+let view = vide { article { main { cards } } }
 
 
 //module Issue_2b =
